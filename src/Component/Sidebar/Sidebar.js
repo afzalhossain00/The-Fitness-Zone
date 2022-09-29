825e7fff1792c7813dd4b27383da4d778cc32bab
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-09-29 14:33:21 +0600

    added sidebar design

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -4,7 +4,35 @@ import './Sidebar.css'
 const Sidebar = ({ sidebar }) => {
     return (
         <div className='sidebar'>
-            <h3>Profile</h3>
+            <div className='profile'>
+                <h3>Afzal Hossain</h3>
+                <h5 className='profile-position'>Web Developer</h5>
+            </div>
+            <div >
+                <h3>Add A Break</h3>
+            </div>
+            <div className='btn-container'>
+                <button className='btn'>10s</button>
+                <button className='btn'>20s</button>
+                <button className='btn'>30s</button>
+                <button className='btn'>40s</button>
+            </div>
+            <div>
+                <h3>Exercise Details</h3>
+                <div className='exercise-time'>
+                    <h4>Exercise Time: </h4>
+                    <h4>{ }Seconds</h4>
+                </div>
+                <div className='break-time'>
+                    <h4>Break Time: </h4>
+                    <h4>{ } Seconds</h4>
+                </div>
+            </div>
+            <div>
+                <button className='btn-activity'>
+                    <p>Activity Completed</p>
+                </button>
+            </div>
         </div>
     );
 };
