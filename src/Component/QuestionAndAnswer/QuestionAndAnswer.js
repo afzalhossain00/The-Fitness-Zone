19e1a688bf8d5f11cf8e9aad742a3d2512aa250d
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-09-29 23:29:59 +0600

    final commit

--- src/Component/QuestionAndAnswer/QuestionAndAnswer.js
+++ src/Component/QuestionAndAnswer/QuestionAndAnswer.js
@@ -7,7 +7,7 @@ const QuestionAndAnswer = () => {
             <h3>Question And Answer:</h3>
             <div className='answer'>
                 <h4>1. How does React work?</h4>
-                <p>React just maintains a tree for you and it will do efficient diff computations on the nodes. It allows you to effectively re-construct your DOM in JavaScript and push only those changes to the DOM which have actually occurred..
+                <p>React just maintains a tree for you and it will do efficient diff computations on the nodes. It allows you to effectively re-construct your DOM in JavaScript and push only those changes to the DOM which have actually occurred.
                 </p>
 
             </div>
@@ -19,7 +19,7 @@ const QuestionAndAnswer = () => {
             </div>
             <div className='answer'>
                 <h4>3. What does useEffect do without load API through fetch?</h4>
-                <p>Accepts a function that contains imperative possibly effectful code. Mutations, subscriptions, timers, logging, and other side effects are not allowed inside the main body of a function component. useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
+                <p>The useEffect hook allows components to react to lifecycle events such as mounting to the DOM, re-rendering, and unmounting. useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
                 </p>
 
             </div>
